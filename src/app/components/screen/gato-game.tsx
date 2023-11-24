2760f7d706fa283312f327b2342ecc632e53fbca
Jasson Fernandez <jeicobfer@gmail.com>
2023-11-23 22:40:35 -0600

    creando estructura de trabajo

---
+++ src/app/components/screen/gato-game.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GatoGame() {
+  return (
+    <div
+      className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="text-4xl text-#000" >Gato Game</h1>
+      
+    </div>
+  );
+}
